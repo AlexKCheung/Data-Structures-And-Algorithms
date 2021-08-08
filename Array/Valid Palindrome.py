dad9743b35656ae752a5ff28207efe7411dc4b13
Alex <alexcheung880@gmail.com>
2021-08-07 20:11:14 -0700

    more clean and efficient solution for valid palindrome problem

--- Array/Valid Palindrome.py
+++ Array/Valid Palindrome.py
@@ -4,6 +4,28 @@ Given a string s, determine if it is a palindrome,
 considering only alphanumeric characters and ignoring cases.
 '''
 def isPalindrome(self, s: str) -> bool:
+        # two pointer solution
+        # linear timer and constant space as comparisons are done in place
+        left = 0
+        right = len(s) -1
+        while left < right: 
+            if not s[left].isalnum(): 
+                left += 1
+                continue
+            elif not s[right].isalnum(): 
+                right -= 1
+                continue
+                
+            if s[left].lower() == s[right].lower():
+                left += 1
+                right -= 1
+                continue
+            else: 
+                return False
+        return True
+                
+        
+        '''
         # ignore spaces and special characters like ,.:
         # first convert into array 
         characters = list()
@@ -14,11 +36,13 @@ def isPalindrome(self, s: str) -> bool:
                 characters.append(i.lower())
                 count += 1
             '''
+        '''
             a-z and A-Z
             if i.lower() >= 'a' and i.lower() <= 'z': 
-                characte    rs.append(i.lower())
+                characters.append(i.lower())
                 count += 1
             '''
+        '''
         
         # check if array is a palindrome 
         for i in range(len(characters) // 2): 
@@ -28,3 +52,4 @@ def isPalindrome(self, s: str) -> bool:
         return True
     # time would be O(n) for linear scan of s
     # space would be O(n) for s
+    '''
